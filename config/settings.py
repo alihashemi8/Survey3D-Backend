4c1065d41d118ff449e0cab583b7750749ecb331
ali <alihashemi5553@gmail.com>
2025-08-17 22:26:01 +0330

    liara2

--- config/settings.py
+++ config/settings.py
@@ -65,9 +65,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 # پایه لیست Origins
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://survey-ce.liara.run",
 ]
